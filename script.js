e7a1a30e3316b0366d8a7d801539e4d935a44f16
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2021-09-04 12:24:32 +0530

    stock qty

---
+++ script.js
@@ -0,0 +1,50 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQty = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var outputBox = document.querySelector("#output-box");
+var submitBtn = document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click", calculateProfitAndLoss);
+
+var submitHandler = (e) => {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQty.value);
+  var curr = Number(currentPrice.value);
+  if (
+    (ip == null || ip == "",
+    qty == null || qty == "" || curr == null || curr == "")
+  ) {
+    showOutputMessage("please enter all details");
+  } else {
+    calculateProfitAndLoss(ip, qty, curr);
+  }
+};
+
+submitBtn.addEventListener("click", submitHandler);
+
+//ex-05
+var calculateProfitAndLoss = (initial, quantity, current) => {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercent = (loss / initial) * 100;
+    showOutputMessage(
+      `Hey the loss is ${lossPercent.toFixed(
+        2
+      )} and the percent is ${lossPercent.toFixed(2)} %`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercent = (profit / initial) * 100;
+    showOutputMessage(
+      `Hey the profit is ${profit.toFixed(
+        2
+      )} and the profit percentage is ${profitPercent.toFixed(2)} %`
+    );
+  } else {
+    showOutputMessage("No pain no gain and no gain and no pain");
+  }
+};
+
+var showOutputMessage = (str) => {
+  outputBox.innerText = str;
+};
