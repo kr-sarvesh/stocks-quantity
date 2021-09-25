b1e02ebe5d63970b380d7efa199f0d1836b95d6d
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2021-09-25 23:21:21 +0530

    fixed: code fixed

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-var initialPrice = document.querySelector("#initial-price");
-var stocksQty = document.querySelector("#stocks-quantity");
-var currentPrice = document.querySelector("#current-price");
-var outputBox = document.querySelector("#output-box");
-var submitBtn = document.querySelector("#submit-btn");
+var initialPrice = document.querySelector('#initial-price');
+var stocksQty = document.querySelector('#stocks-quantity');
+var currentPrice = document.querySelector('#current-price');
+var outputBox = document.querySelector('#output-box');
+var submitBtn = document.querySelector('#submit-btn');
+var form = document.querySelector('.form-handler');
 
-submitBtn.addEventListener("click", calculateProfitAndLoss);
+form.addEventListener('submit', calculateProfitAndLoss);
 
 var submitHandler = (e) => {
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQty.value);
   var curr = Number(currentPrice.value);
   if (
-    (ip == null || ip == "",
-    qty == null || qty == "" || curr == null || curr == "")
+    (ip == null || ip == '',
+    qty == null || qty == '' || curr == null || curr == '')
   ) {
-    showOutputMessage("please enter all details");
+    showOutputMessage('please enter all details');
+  } else if (curr < 0) {
+    showOutputMessage('Enter positive value');
   } else {
     calculateProfitAndLoss(ip, qty, curr);
   }
 };
 
-submitBtn.addEventListener("click", submitHandler);
+submitBtn.addEventListener('click', submitHandler);
 
 //ex-05
 var calculateProfitAndLoss = (initial, quantity, current) => {
@@ -41,7 +44,7 @@ var calculateProfitAndLoss = (initial, quantity, current) => {
       )} and the profit percentage is ${profitPercent.toFixed(2)} %`
     );
   } else {
-    showOutputMessage("No pain no gain and no gain and no pain");
+    showOutputMessage('No pain no gain and no gain and no pain');
   }
 };
 
